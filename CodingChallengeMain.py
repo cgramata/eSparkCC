6c29d0c7c47a41413589e7e64c632a08c139c74a
Carl Gramata <carlgramata@Carls-MacBook-Pro.local>
2017-06-19 23:00:13 -0500

    Adds code that reads the file as a csv
    Also puts each domain row into a dictionary

--- CodingChallengeMain.py
+++ CodingChallengeMain.py
@@ -1,14 +1,32 @@
+import csv
+
+
 def importFiles():
+	domainDictionary = {}
+
 	domainFile = open("domain_order.csv")
 	scoresFiles = open("student_tests.csv")
 
-	for row in domainFile:
-		print(row)
+	domainFileReader = csv.reader(domainFile, delimiter=',')
+
+	for row in domainFileReader:
+		row = filter(None, row)
+		storeDomainValues(row, domainDictionary)
+
+	print(domainDictionary)
 
 	domainFile.close()
 
 
+def storeDomainValues(rowParam, aDictionary):
+	localDomainDictionary = aDictionary
+
+	localDomainDictionary[rowParam[0]] = rowParam[1:]
+
+
+
 def main():
+
 	importFiles()
 
 if __name__ == "__main__": main()
